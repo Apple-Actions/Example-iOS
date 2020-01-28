f92e8e2212bc722ce388d5ab5bac8fae7c8c2e66
Oliver Jones <oliver@deeperdesign.com>
2020-01-28 20:07:58 +1100

    Initial code import

---
+++ Example iOS/Example iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example iOS
+//
+//  Created by Oliver Jones on 28/1/20.
+//  Copyright © 2020 Oliver Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Example iOS/Example iOSTests/Example_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Example_iOSTests.swift
+//  Example iOSTests
+//
+//  Created by Oliver Jones on 28/1/20.
+//  Copyright © 2020 Oliver Jones. All rights reserved.
+//
+
+import XCTest
+@testable import Example_iOS
+
+class Example_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
